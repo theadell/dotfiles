1a88f34e2ebb1fe6d4b2f76b0fdc5a8b70f13290
theadell <65723807+theadell@users.noreply.github.com>
2025-01-19 10:49:23 +0100

    Initial dotfiles commit

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,24 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+-- line wrapping
+opt.wrap = false -- disable line wrapping
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
+
+-- clipboard integration 
+opt.clipboard:append("unnamedplus")
