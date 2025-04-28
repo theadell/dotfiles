a3db5e38c4833e0bac5acc01daaddb0d32937818
theadell <65723807+theadell@users.noreply.github.com>
2025-04-28 18:39:27 +0200

    use oil.nvim

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -30,6 +30,9 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>")     --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>")     --  go to previous tab
 
+-- file explorer
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
 
 vim.keymap.set('t', '<leader><Esc>', [[<C-\><C-N>]], { desc = "Exit Terminal mode" })
 
---
+++ nvim/lua/plugins/go.lua
@@ -0,0 +1,17 @@
+return {
+  "ray-x/go.nvim",
+  dependencies = {
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("go").setup({
+      -- use lsp config in lsp/servers/gopls.lua
+      lsp_cfg = false
+    })
+  end,
+  event = { "CmdlineEnter" },
+  ft = { "go", 'gomod' },
+  build = ':lua require("go.install").update_all_sync()'
+}
