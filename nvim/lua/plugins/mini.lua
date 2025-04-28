9b34e1296b61101787e05d19c831eb1886342b96
theadell <65723807+theadell@users.noreply.github.com>
2025-04-28 23:00:45 +0200

    use mini.nvim

---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,9 @@
+return {
+  'echasnovski/mini.nvim',
+  version = false,
+  config = function()
+    require('mini.surround').setup()
+    require('mini.ai').setup()
+    require("mini.comment").setup()
+  end,
+}
